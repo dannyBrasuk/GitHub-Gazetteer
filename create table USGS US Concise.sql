9a3bc7ecb868b00a8c75381c9ae7e29d6b91b291
dannyBrasuk <danny.brasuk@PeachtreeGeographics.com>
2014-10-03 15:39:36 -0400

    Forgot four tables. Opps.

---
+++ create table USGS US Concise.sql
@@ -0,0 +1,35 @@
+/*
+Import the flat file of Concise  into this "work" table.  
+
+It's only use is to initialize the "Display on Map Flag" column of the Feature Guide table.
+September 2014
+
+*/
+
+Use Gazetteer;
+GO
+CREATE TABLE USGS.[Concise] 
+ ( 
+	[FEATURE_ID]     	            int NULL,
+	[FEATURE_NAME]   	varchar(120) NULL,
+	[FEATURE_CLASS]  	varchar(50) NULL,
+	[STATE_ALPHA]    	            varchar(2) NULL,
+	[STATE_NUMERIC]  	varchar(2) NULL,
+	[COUNTY_NAME]    	varchar(100) NULL,
+	[COUNTY_NUMERIC] 	varchar(3) NULL,
+	[PRIMARY_LAT_DMS]	varchar(7) NULL,
+	[PRIM_LONG_DMS]  	varchar(8) NULL,
+	[PRIM_LAT_DEC]        	numeric(11,7) NULL,
+	[PRIM_LONG_DEC]  	numeric(12,7) NULL,
+	[SOURCE_LAT_DMS] 	varchar(7) NULL,
+	[SOURCE_LONG_DMS]	varchar(8) NULL,
+	[SOURCE_LAT_DEC] 	numeric(11,7) NULL,
+	[SOURCE_LONG_DEC]	numeric(12,7) NULL,
+	[ELEV_IN_M]      	int NULL,
+	[MAP_NAME]       	varchar(100) NULL,
+	[DATE_CREATED]   date NULL,
+	[DATE_EDITED]    	date NULL 
+	)
+ON [Secondary]
+WITH (DATA_COMPRESSION = NONE)
+GO
