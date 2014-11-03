35234ced7d8b7bf688184b2e864dd52742ed3aad
dannyBrasuk <danny.brasuk@PeachtreeGeographics.com>
2014-11-03 15:49:11 -0800

    Search manage in good working order now.  Progressing on name search

---
+++ create proc App Gazetteer Controller.sql
@@ -0,0 +1,55 @@
+USE Gazetteer;
+GO
+IF OBJECT_ID('App.GazetteerController') IS NOT NULL
+        DROP PROCEDURE [App].[GazetteerController];
+GO
+CREATE PROCEDURE [App].[GazetteerController]
+@Debug BIT = 0
+
+AS
+
+SET NOCOUNT ON;
+
+DECLARE 
+    @RC INT = 0
+    ,@ErrorMessage VARCHAR(MAX) = ''
+    ,@ProcedureName VARCHAR(MAX) = OBJECT_NAME(@@PROCID)
+    ,@ParameterSet VARCHAR(MAX) = ''
+    ,@StatusMessage VARCHAR(MAX) = ''
+    ,@ProcedureLog_fk INT = 0 
+;
+
+BEGIN
+
+	BEGIN TRY
+
+                EXEC [App].[ProcedureLog_Merge] @ProcedureLog_fk = @ProcedureLog_fk OUT, @ParameterSet = @ParameterSet, @StatusMessage = @StatusMessage, @ProcedureName = @ProcedureName;
+
+
+
+               SET @StatusMessage =+ App.fnStatusMessage(@StatusMessage,'Completed.', @RC);
+               EXEC [App].[ProcedureLog_Merge] @ProcedureLog_fk = @ProcedureLog_fk, @StatusMessage = @StatusMessage, @ReturnCode = @RC;
+   
+	END TRY
+  
+	BEGIN CATCH
+ 
+		SET @RC = -1;
+                        SET @StatusMessage = 'Error';
+		EXEC [App].[Errors_GetInfo] @Message = @ErrorMessage OUT, @PrintMessage = 0;
+
+		EXEC [App].[ProcedureLog_Merge]
+				@ProcedureLog_fk = @ProcedureLog_fk OUT,
+				@ProcedureName = @ProcedureName,
+				@StatusMessage = @StatusMessage,
+				@ErrorMessage = @ErrorMessage,
+				@ReturnCode = @RC;
+
+	END CATCH
+
+RETURN(@RC)
+
+END
+
+GO
+
